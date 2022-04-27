3b1cdca88cd972dcd2a8f860b869075aa4799bdc
Klaas Cuvelier <cuvelierklaas@gmail.com>
2022-04-27 16:00:53 +0200

    chore: upgrade to nx 14

---
+++ packages/nx-deno-deploy/jest.config.ts
@@ -0,0 +1,16 @@
+module.exports = {
+    displayName: 'nx-deno-deploy',
+
+    globals: {
+        'ts-jest': {
+            tsconfig: '<rootDir>/tsconfig.spec.json',
+        },
+    },
+    testEnvironment: 'node',
+    transform: {
+        '^.+\\.[tj]sx?$': 'ts-jest',
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+    coverageDirectory: '../../coverage/packages/nx-deno-deploy',
+    preset: '../../jest.preset.ts',
+};
---
+++ packages/nx-rsync-deployer/jest.config.ts
@@ -0,0 +1,16 @@
+module.exports = {
+    displayName: 'nx-rsync-deployer',
+
+    globals: {
+        'ts-jest': {
+            tsconfig: '<rootDir>/tsconfig.spec.json',
+        },
+    },
+    testEnvironment: 'node',
+    transform: {
+        '^.+\\.[tj]sx?$': 'ts-jest',
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+    coverageDirectory: '../../coverage/packages/nx-rsync-deployer',
+    preset: '../../jest.preset.ts',
+};
