58b72bf17f4695f9bd41de5fe9c73d8c04503317
= <cuvelierklaas@gmail.com>
2024-06-10 13:35:21 +0200

    chore: new publish script (wip)

---
+++ tools/publish.ts
@@ -0,0 +1,23 @@
+import { execSync } from 'child_process';
+
+execSync('npx nx release --skip-publish', { stdio: 'inherit' });
+const otp = getOtp();
+console.log('OTP,', otp);
+execSync(`npx nx release publish --otp=${otp}`, { stdio: 'inherit' });
+
+function getOtp() {
+    try {
+        execSync(`which op`);
+    } catch (e) {
+        throw new Error(`Could not invoke OnePassword CLI.`);
+    }
+
+    try {
+        return execSync(`eval $(op signin) && op item get Npmjs --otp`, {
+            stdio: ['inherit', 'pipe', 'inherit'],
+            encoding: 'utf-8',
+        }).trim();
+    } catch (e) {
+        throw new Error(`Could not get one time password for npmjs: ` + e);
+    }
+}
